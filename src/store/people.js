0fbb04db33c8b8b213a45a60126af5165cb6c7c4
Princewill Okoroafor <Princewill Okoroafor>
2020-07-15 23:49:27 -0700

    added team bios

--- src/store/people.js
+++ src/store/people.js
@@ -37,7 +37,7 @@ const people = [
     // },
     {
         active: true,
-        bio: "Hey, I am Princewill Okoroafor. ",
+        bio: "Graduate of Mathematics and Computer Science at Harvey Mudd College and a first year PhD student at Cornell. He has a silver medal from the PAMO and an honourable mention from the IMO. He is passionate about Mathematics and Computer science education and improving accessibility to quality educational content. In his free time, he enjoys playing soccer and chess. He currently works as an intern at Microsoft.",
         createdBy: {
             id: "princewill_okoroafor",
             name: "Princewill Okoroafor"
@@ -82,10 +82,10 @@ const people = [
             nanoseconds: 249000000,
             seconds: 1590076439
         },
-        designation: "Nigeria",
+        designation: "Cofounder and Instructor",
         email: "",
         id: "animoku_abdulwahab",
-        image: "",
+        image: "https://i1.rgstatic.net/ii/profile.image/612664891949056-1523082101362_Q512/Abdulwahab_Animoku.jpg",
         lastUpdatedBy: {
             id: "",
             name: ""
@@ -95,6 +95,181 @@ const people = [
         name: "Animoku Abdulwahab",
         password: "",
         role: "Core Team",
+        socialLinks: {
+            facebook: "",
+            github: "",
+            linkedin: "",
+            medium: "",
+            twitter: "",
+            web: "",
+        },
+        uid: "F1nj86BQKzgCA8BDcnm6DMMBKq83",
+        visible: true
+    },
+    {
+        active: true,
+        bio: "Graduate of Computer Science from Stanford University. He is a one-time Silver medallist at PAMO and a 1-time honourable mentionee at the IMO. He is passionate about Mathematics education and improving accessibility to quality educational content. In his free time, he enjoys listening to music and watching football. He currently works at Google.",
+        createdBy: {
+            id: "princewill_okoroafor",
+            name: "Princewill Okoroafor"
+        },
+        createdOn: {
+            nanoseconds: 249000000,
+            seconds: 1590076439
+        },
+        designation: "Cofounder and Instructor",
+        email: "",
+        id: "oluwasanya_awe",
+        image: "https://imo-official.org/photos/Tempupload/Thumb_NGA_20140606-002938-953.jpg",
+        lastUpdatedBy: {
+            id: "",
+            name: ""
+        },
+        lastUpdatedOn: "",
+        mbnumber: "",
+        name: "Oluwasanya Awe",
+        password: "",
+        role: "Core Team",
+        socialLinks: {
+            facebook: "",
+            github: "",
+            linkedin: "",
+            medium: "",
+            twitter: "",
+            web: "",
+        },
+        uid: "F1nj86BQKzgCA8BDcnm6DMMBKq83",
+        visible: true
+    },
+    {
+        active: true,
+        bio: "Graduate of Mathematics from the University of Cambridge and has been actively involved in the Olympiads for over ten years. First Nigerian IMO bronze medalist, one-time silver and one-time gold medalist at PAMO. Two-time deputy team leader at IMO and two-time deputy team leader at PAMO. Team trainer for the Nigerian IMO team for the past 4 years. He currently works as an Assistant research fellow at National Mathematical Centre and acts as a resource person for Special Maths Academy Limited.",
+        createdBy: {
+            id: "princewill_okoroafor",
+            name: "Princewill Okoroafor"
+        },
+        createdOn: {
+            nanoseconds: 249000000,
+            seconds: 1590076439
+        },
+        designation: "Cofounder and Instructor",
+        email: "",
+        id: "pius_onah",
+        image: "https://www.imo-official.org/photos/2011/ThumbImage4608.jpg",
+        lastUpdatedBy: {
+            id: "",
+            name: ""
+        },
+        lastUpdatedOn: "",
+        mbnumber: "",
+        name: "Pius Onah",
+        password: "",
+        role: "Core Team",
+        socialLinks: {
+            facebook: "",
+            github: "",
+            linkedin: "",
+            medium: "",
+            twitter: "",
+            web: "",
+        },
+        uid: "F1nj86BQKzgCA8BDcnm6DMMBKq83",
+        visible: true
+    },
+    {
+        active: true,
+        bio: "​Currently studying Mathematics at the Peoples’ Friendship University, Moscow, Russia. During his secondary school days, he represented Nigeria three times in IMO, winning 2 Honourable Mentions Awards. He is also a two-time PAMO Gold Medalist. In 2017, he finished in the top 5% of contestants in the American Mathematics Olympiad (AMC 12) and qualified for the prestigious AIME (American Invitational Mathematics Examination). Aside from Mathematics, Williams also likes engaging in critical thinking, playing video games and watching football matches.",
+        createdBy: {
+            id: "princewill_okoroafor",
+            name: "Princewill Okoroafor"
+        },
+        createdOn: {
+            nanoseconds: 249000000,
+            seconds: 1590076439
+        },
+        designation: "Cofounder and Instructor",
+        email: "",
+        id: "williams_okeke",
+        image: "https://imo-official.org/photos/Tempupload/Thumb_NGA_20160513-071653-371.jpg",
+        lastUpdatedBy: {
+            id: "",
+            name: ""
+        },
+        lastUpdatedOn: "",
+        mbnumber: "",
+        name: "Williams Okeke",
+        password: "",
+        role: "Core Team",
+        socialLinks: {
+            facebook: "",
+            github: "",
+            linkedin: "",
+            medium: "",
+            twitter: "",
+            web: "",
+        },
+        uid: "F1nj86BQKzgCA8BDcnm6DMMBKq83",
+        visible: true
+    },
+    {
+        active: true,
+        bio: "1-time special medal recipient for AMC10, and 5-time AMC national champion. 2 time silver medalist at PAMO and 3 time honourable mentionee at IMO. Mmesomachi has been involved extensively in Olympiad training at Graceland International School, while as a student and currently an undergraduate student of Mathematics in People’s Friendship University of Russia.​",
+        createdBy: {
+            id: "princewill_okoroafor",
+            name: "Princewill Okoroafor"
+        },
+        createdOn: {
+            nanoseconds: 249000000,
+            seconds: 1590076439
+        },
+        designation: "Cofounder and Instructor",
+        email: "",
+        id: "mmesomachi_nwachukwu",
+        image: "https://www.imo-official.org/photos/2015/ThumbImage9907.jpg",
+        lastUpdatedBy: {
+            id: "",
+            name: ""
+        },
+        lastUpdatedOn: "",
+        mbnumber: "",
+        name: "Mmesomachi Nwachukwu",
+        password: "",
+        role: "Core Team",
+        socialLinks: {
+            facebook: "",
+            github: "",
+            linkedin: "",
+            medium: "",
+            twitter: "",
+            web: "",
+        },
+        uid: "F1nj86BQKzgCA8BDcnm6DMMBKq83",
+        visible: true
+    },
+    {
+        active: true,
+        bio: "",
+        createdBy: {
+            id: "princewill_okoroafor",
+            name: "Princewill Okoroafor"
+        },
+        createdOn: {
+            nanoseconds: 249000000,
+            seconds: 1590076439
+        },
+        designation: "Associate and Instructor",
+        email: "",
+        id: "henry_aniobi",
+        image: "https://imo-official.org/photos/Tempupload/Thumb_NGA_20140528-233215-934.jpg",
+        lastUpdatedBy: {
+            id: "",
+            name: ""
+        },
+        lastUpdatedOn: "",
+        mbnumber: "",
+        name: "Henry Aniobi",
+        password: "",
+        role: "Organizing Team",
         socialLinks: {
             facebook: "",
             github: "",
@@ -148,7 +323,7 @@ const people = [
 
 const instructors = [
     {
-        bio: "Hi, I am Princewill",
+        bio: "Graduate of Mathematics and Computer Science at Harvey Mudd College and a first year PhD student at Cornell. He has a silver medal from the PAMO and an honourable mention from the IMO. He is passionate about Mathematics and Computer science education and improving accessibility to quality educational content. In his free time, he enjoys playing soccer and chess. He currently works as an intern at Microsoft.",
         city: "Washington",
         company: {name: "Microsoft", url: ""},
         country: "United States",
@@ -195,11 +370,12 @@ const instructors = [
 
 const team = [
     "animoku_abdulwahab",
-    "awe_oluwasanya",
+    "oluwasanya_awe",
+    "pius_onah",
     "mmesomachi",
-    "okeke_williams",
+    "williams_okeke",
     "princewill_okoroafor",
-    "princewill_okoroafor2",
+    "mmesomachi_nwachukwu",
 ]
 
 export { team, instructors, people };
